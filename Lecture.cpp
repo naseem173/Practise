2667f0dd564b3e939f4f79793e20c518bb182b35
naseem173 <md.naseem0978@gmail.com>
2023-07-03 22:38:15 +0530

    added few changes

--- Lecture.cpp
+++ Lecture.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 using namespace std;
 
+// int main(){
+//     for(int i=0;i<10;i++){
+//     cout<<"hello world!"<<endl;
+//     }
+// }
+
 int main(){
-    for(int i=0;i<10;i++){
-    cout<<"hello world!"<<endl;
+    int res;
+    cout<<"Enter percentage";
+    cin>>res;
+    if(res>=50 && res<75){
+        cout<<"average"<<endl;
     }
+    else if(res>=75){
+        cout<<"First Class"<<endl;
+    }
+    else
+        cout<<"Failed"<<endl;
+    return 0;
 }
